a6ddc98592fc4174c96e539d55c0702ba7be675d
Howard <howardl93@yahoo.com>
2021-04-09 16:10:34 -0700

    added package.json script and streamlined to include command line input

--- webdata.js
+++ webdata.js
@@ -1,8 +1,25 @@
 
 var axios = require("axios");
 
-const getAuthorData = () =>{
-    let url = "https://archiveofourown.org/users/iluv2eat/pseuds/iluv2eat/works";
+
+// const getAuthorData = () =>{
+//     let url = "https://archiveofourown.org/users/iluv2eat/pseuds/iluv2eat/works";
+
+//     //let url = `https://archiveofourown.org/users/${user}/pseuds/iluv2eat/works`;
+
+//     const res = axios.get(url);
+    
+//     return res;
+
+// }
+
+
+
+
+
+const getAuthorData = (user) =>{
+
+    let url = `https://archiveofourown.org/users/${user}/pseuds/iluv2eat/works`;
 
     const res = axios.get(url);
     
