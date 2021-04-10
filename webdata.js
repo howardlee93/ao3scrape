d4cf73a1ad5545d298943d00d48efc254a293447
Howard <howardl93@yahoo.com>
2021-04-09 20:47:40 -0700

    cleanup

--- webdata.js
+++ webdata.js
@@ -1,22 +1,6 @@
 
 var axios = require("axios");
 
-
-// const getAuthorData = () =>{
-//     let url = "https://archiveofourown.org/users/iluv2eat/pseuds/iluv2eat/works";
-
-//     //let url = `https://archiveofourown.org/users/${user}/pseuds/iluv2eat/works`;
-
-//     const res = axios.get(url);
-    
-//     return res;
-
-// }
-
-
-
-
-
 const getAuthorData = (user) =>{
 
     let url = `https://archiveofourown.org/users/${user}/pseuds/iluv2eat/works`;
